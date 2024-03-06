46198277b99b8f438789182ba22d4cb620cf34c8
kaurr455 <156359067+kaurr455@users.noreply.github.com>
2024-03-06 01:07:55 -0500

    Add files via upload

---
+++ Assignment_4.js
@@ -0,0 +1,7 @@
+for (let number = 0; number <= 200; number++) {
+    if (number % 2 === 0) {
+        console.log(`${number} is even.`);
+    } else {
+        console.log(`${number} is odd.`);
+    }
+}
